8379d353eabe50c2ad67f077c68eca55c68f75a6
Lyllyanny França <lyllyanny.franca@gmail.com>
2021-05-07 20:33:40 -0300

    projeto inicializado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace anima_channel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
