7c1d167ebc0af11a958e843aa2468b316ceacfd8
Lyllyanny França <lyllyanny.franca@gmail.com>
2021-05-08 12:36:08 -0300

    projeto base finalizado.

---
+++ Classes/Serie.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anima_channel
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id; //modificar depois para Id++
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excluido = false;
+        }
+
+        public string retornaTitulo()
+        {
+            return Titulo;
+        }
+
+        public bool retornaExcluido()
+        {
+            return Excluido;
+        }
+
+        internal int retornaId()
+        {
+            return Id;
+        }
+
+        public void Excluir()
+        {
+            Excluido = true;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + Genero + Environment.NewLine; //Environment.NewLine = Interpreta como um S.O representa uma quebra de linha.
+            retorno += "Título: " + Titulo + Environment.NewLine;
+            retorno += "Descrição: " + Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + Ano;
+            return retorno;
+        }
+
+    }
+}
---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using anima_channel.Interfaces;
+
+namespace anima_channel
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualizar(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Excluir(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Inserir(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Listar()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornarPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
